68aac0673bbce614ece8f74f42c2215a42d992b0
Simun-12 <simunsarangi0912@gmail.com>
2023-01-05 03:07:50 +0530

    navbar update

--- src/Components/Nabvar/Navbar.jsx
+++ src/Components/Nabvar/Navbar.jsx
@@ -2,35 +2,41 @@ import React from "react";
 import {Link} from 'react-router-dom'
 import "./navbar.css"
 import { BsFillBookFill } from 'react-icons/bs';
+import { useState } from "react";
 
 
 
 function Navbar(){
 
- 
-    
+  const[color,setColor] = useState(false)
+  const changeColor = () =>{
+      if(window.scrollY >= 100)
+        {setColor(true);}
+        else {setColor(false);}
+  };
+    window.addEventListener("scroll",changeColor);
     return(
      <div className="Navbar">
         
        <div className="">
-          <nav className="main">
+          <nav className={color?'main2':'main1'}>
             <div className="link"> 
                   <div className="port"  >
-                      <Link to="/" className="logo">
-                      <h2 className="heading"><BsFillBookFill size={35} className="log" /></h2></Link> 
+                      <Link to="/" >
+                      <h2 className="heading"><BsFillBookFill size={35} className={color?"log1":"log"} /></h2></Link> 
                   
               <ul>
                 <li>
-                <Link to='/' className="navigation">Home </Link> 
+                <Link to='/' className={color?"navigation1":"navigation"}>Home </Link> 
                 </li>
                 <li>
-                <Link to='./search' className="navigation">Search </Link>
+                <Link to='./search' className={color?"navigation1":"navigation"}>Search </Link>
                 </li>
                 <li>
-                <Link to='./login' className="navigation">Login</Link>
+                <Link to='./login' className={color?"navigation1":"navigation"}>Login</Link>
                 </li>
                 <li>
-                <Link to='./chat' className="navigation">Chat </Link>
+                <Link to='./chat' className={color?"navigation1":"navigation"}>Chat </Link>
                 </li>
               </ul>
               </div>
--- src/Components/SearchPage/SearchPage.jsx
+++ src/Components/SearchPage/SearchPage.jsx
@@ -1,10 +1,57 @@
 import React from "react";
+import bg1img from './bg1.jpeg';
+import SearchCard from "./SearchCard";
+import { useState } from "react";
+import axios from "axios";
+import './SearchPage.css'
+
 
 function Search (){
+    const [search,setSearch]=useState("");
+    const [bookData,setData]=useState([]);
+  
+    const searchBook=(evt)=>{
+        if(evt.key==="Enter")
+        {
+            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU'+'&maxResults=40')
+            .then(res=>setData(res.data.items))
+            .catch(err=>console.log(err))
+        }
+    }
+
+    const searchBoob=()=>{
+      
+            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyA6SaT23KNiiA6DnUfUQTvFeyAcQEkwnSU'+'&maxResults=40')
+            .then(res=> console.log(res))
+            .catch(err=>console.log(err))
+        }
+    
+
     return(
-       <div>
-        fiuerteoi
-       </div>
-    );
+        <>
+            <div className="header">
+                <div className="row1">
+                    <h1>A room without books is like<br/> a body without a soul.</h1>
+                </div> 
+                <div className="row2">
+                    <h2>Find Your Book</h2>
+                    <div className="search">
+                        <input type="text" placeholder="Enter Your Book Name"
+                        value={search} onChange={e=>setSearch(e.target.value)}
+                        onKeyPress ={searchBook}/>
+                        <button onClick={searchBoob}  ><i  className="fas fa-search"></i></button>
+                    </div>
+                    <img src ={bg1img} alt="" />
+                </div>
+            </div>
+
+            <div className="container1">
+              {
+                    <SearchCard book={bookData}/>
+              }  
+            </div>
+           
+        </>
+    )
 }
 export default Search;
